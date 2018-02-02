529440540f14056d85377b0320cf163eaa45835c
Alexsandropolys <Veelzevul@mail2000.ru>
2018-02-02 17:40:11 +0700

    Добавьте файлы проекта.

---
+++ Test/Getter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleApi
+{
+    public class Getter
+    {
+       const string RequestBeginString = "http://apilayer.net/api/historical?access_key=e5d34f2b8f962721980f27abb0d02313&source=USD&date=";
+       public static double GetValue(DateTime date)
+        {
+            string line = string.Empty;
+            string dateForRequest = date.ToString("yyyy-MM-dd");
+            WebRequest request = WebRequest.Create(RequestBeginString + dateForRequest);
+            WebResponse response = request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            using (StreamReader sr = new StreamReader(stream))
+            {
+                line = sr.ReadToEnd();
+            }
+            JObject resp = JsonConvert.DeserializeObject(line) as JObject;
+            double usd = Convert.ToDouble(resp["quotes"]["USDALL"]);
+
+            return usd;
+        }
+        
+        public static double[] RequestForMonth(DateTime date)
+        {
+            double[] result = new double[25];
+            for(int i = 0; i < 25; i++)
+            {
+                DateTime currentDate = date.AddDays(i);
+                result[i] = GetValue(currentDate);
+            }
+            return result;
+        }
+    }
+}
---
+++ Test/Tutorial.cs
@@ -0,0 +1,68 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using System;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using ConsoleApi;
+using Autodesk.AutoCAD.EditorInput;
+
+namespace Test
+{
+    public class Tutorial : IExtensionApplication
+    {
+        [CommandMethod("ShowUSDCourse")]
+        public void Helloworld()
+        {
+            Editor editor = Application.DocumentManager.MdiActiveDocument.Editor;
+            editor.WriteMessage("Привет из Autocad плагина");
+
+            PromptResult pr = editor.GetString("\nProvide a date in format YYYY.MM : ");
+            if (pr.Status != PromptStatus.OK)
+            {
+                editor.WriteMessage("No string was provided\n");
+                
+                return;
+            }
+            
+            using (Polyline acPoly = new Polyline())
+            {
+                DateTime date;
+                DateTime.TryParse(pr.StringResult, out date);
+                double[] array = Getter.RequestForMonth(date);
+                int counter = 0;
+                foreach (double a in array)
+                {
+                    acPoly.AddVertexAt(counter, new Point2d(counter++ *3, a*10 - array[0]*10), 0, 0, 0);
+                }
+
+                Database acadDB = HostApplicationServices.WorkingDatabase;
+                Autodesk.AutoCAD.DatabaseServices.TransactionManager acadTransMgr = acadDB.TransactionManager;
+                Transaction acadTrans = acadTransMgr.StartTransaction();
+                BlockTableRecord acadBTR = (BlockTableRecord)acadTrans.GetObject(acadDB.CurrentSpaceId, OpenMode.ForWrite);
+                acadBTR.AppendEntity(acPoly);
+                acadTrans.AddNewlyCreatedDBObject(acPoly, true);
+                acadTrans.Commit();
+                Document acDoc = Application.DocumentManager.MdiActiveDocument;
+
+
+                acDoc.SendStringToExecute(".показать границы ", true, false, true);
+            }
+
+
+
+            
+        }
+
+        public void Initialize()
+        {
+            var editor = Application.DocumentManager.MdiActiveDocument.Editor;
+            editor.WriteMessage("Инициализация плагина.." + Environment.NewLine);
+        }
+
+        public void Terminate()
+        {
+
+        }
+
+    }
+}
